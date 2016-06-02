614769af9acbf246c2a8c68926e4e53af41047be
kraken <root@sunsongs.ru>
2016-06-02 19:10:54 +0500

    Removing unused cache configuration

--- src/main/java/ru/sunsongs/auldanov/tradeservice/config/Config.java
+++ src/main/java/ru/sunsongs/auldanov/tradeservice/config/Config.java
@@ -2,10 +2,7 @@ package ru.sunsongs.auldanov.tradeservice.config;
 
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,7 +15,6 @@ import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
-import java.util.Collections;
 
 @Configuration
 @EnableCaching
@@ -36,14 +32,6 @@ public class Config extends WebMvcConfigurerAdapter {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setUseSuffixPatternMatch(false);
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-        simpleCacheManager.setCaches(Collections.singleton(new ConcurrentMapCache("calculationCache")));
-
-        return simpleCacheManager;
     }
 
     @Bean
