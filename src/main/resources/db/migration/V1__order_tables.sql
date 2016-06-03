71a00fc6cf7be2e2f37752ee70af0282b72e770a
kraken <root@sunsongs.ru>
2016-06-03 09:59:55 +0500

    Changing migration script's name to make it in accordance with default template. Removing execution relation from orders

---
+++ src/main/resources/db/migration/V1__order_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE buy_order (
+  id       BIGSERIAL      NOT NULL PRIMARY KEY,
+  quantity BIGINT         NOT NULL,
+  remind   BIGINT         NOT NULL,
+  price    NUMERIC(11, 2) NOT NULL,
+  created  TIMESTAMPTZ    NOT NULL,
+  version  BIGINT
+);
+
+CREATE TABLE sell_order (
+  id       BIGSERIAL      NOT NULL PRIMARY KEY,
+  quantity BIGINT         NOT NULL,
+  remind   BIGINT         NOT NULL,
+  price    NUMERIC(11, 2) NOT NULL,
+  created  TIMESTAMPTZ    NOT NULL,
+  version  BIGINT
+);
+
+CREATE TABLE execution (
+  id            BIGSERIAL   NOT NULL PRIMARY KEY,
+  buy_order_id  BIGINT      NOT NULL,
+  sell_order_id BIGINT      NOT NULL,
+  quantity      BIGINT      NOT NULL,
+  created       TIMESTAMPTZ NOT NULL,
+  CONSTRAINT execution_buy_order_id FOREIGN KEY (buy_order_id) REFERENCES buy_order (id),
+  CONSTRAINT execution_sell_order_id FOREIGN KEY (sell_order_id) REFERENCES sell_order (id)
+);
